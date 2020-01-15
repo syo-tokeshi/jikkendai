ca2d2a31c5679b8d8644bff6f9bb1ab6ba1bbffc
[user_name] <[user_email_address]>
2020-01-15 21:36:30 +0900

    syusyokuapp

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get home" do
+    get root_path
+    assert_response :success
+    # assert_select "title", "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get help" do
+    get help_path
+    assert_response :success
+    # assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+  end
+
+
+
+end
